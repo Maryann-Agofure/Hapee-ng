e8b54c014419f0145341fb8e3b9a0d242cbe63d1
Maryann Agofure <maryannagofure@gmail.com>
2021-05-11 22:09:14 +0100

    Menopause app first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const formSub = document.getElementsByTagName('form')
+        const submitButton = document.getElementsByClassName('sub-button')
+        submitButton.addEventListener('click', function removeDiv(){
+            formSub.style.display = none;
+            const welcomeMessage = document.createElement('p');
+            welcomeMessage.setAttribute('class', 'messaging')
+            welcomeMessage.innerHTML = "Thanks for getting in touch with us. You'd be hearing from us shortly"
+
+            const heading= document.getElementsByClassName('container');
+            heading.appendChild(welcomeMessage);
+            welcomeMessage.style.display = block;
+        })
